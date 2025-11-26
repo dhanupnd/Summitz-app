cc468313d7b1d8c47bd91ccb4167131b8d6dfb37
Dhanu <144932398+dhanupnd@users.noreply.github.com>
2025-11-27 03:40:53 +0700

    Fix import path for TripDetail component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TripDetail from './pages/TripDetail';
+import TripDetail from './pages/tripDetail';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Login from './pages/Login';
